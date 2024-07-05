c217cae362ad660943f9da8551ee85f1f841e870
L1nyz-tel <linyzh36@mail2.sysu.edu.cn>
2024-07-05 15:05:29 +0800

    fix

--- oj-backend-master/src/main/java/com/sysu/oj/mapper/QuestionMapper.java
+++ oj-backend-master/src/main/java/com/sysu/oj/mapper/QuestionMapper.java
@@ -4,7 +4,7 @@ import com.sysu.oj.model.entity.Question;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
-* @author 李鱼皮
+* @author zong
 * @description 针对表【question(题目)】的数据库操作Mapper
 * @createDate 2023-08-07 20:58:00
 * @Entity com.sysu.oj.model.entity.Question
--- oj-backend-master/src/main/java/com/sysu/oj/mapper/QuestionSubmitMapper.java
+++ oj-backend-master/src/main/java/com/sysu/oj/mapper/QuestionSubmitMapper.java
@@ -4,7 +4,7 @@ import com.sysu.oj.model.entity.QuestionSubmit;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
-* @author 李鱼皮
+* @author zong
 * @description 针对表【question_submit(题目提交)】的数据库操作Mapper
 * @createDate 2023-08-07 20:58:53
 * @Entity com.sysu.oj.model.entity.QuestionSubmit
